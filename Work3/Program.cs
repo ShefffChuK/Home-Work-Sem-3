d59b8f52a64e1a77679bd988ee73ca0fdce3b9fc
unknown <sheffchuk-83@list.ru>
2023-03-16 19:59:14 +0500

    Add Work3

---
+++ Work3/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string text)
+{
+System.Console.Write(text);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+void NumberPow(int number)
+{
+for (int i = 1; i <= number; i++)
+{
+System.Console.Write(Math.Pow(i, 3) + " ");
+}
+}
+
+int number = ReadInt("Введите число: ");
+NumberPow(number);
